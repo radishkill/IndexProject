1d0d8cfcc0dffd44ed16db39b06ca7e07b8732ed
radishkill <radishkill@gmail.com>
2022-06-12 14:55:12 +0800

    modify

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,38 @@
-#include <unistd.h>
-#include <pthread.h>
-
+#include <bits/stdc++.h>
 #include <iostream>
-#include <cmath>
-#include <thread>
-#include <functional>
-#include <chrono>
+#include <algorithm>
 
-#include "formula.h"
+void print(const auto &a)
+{
+  for (const auto e : a)
+  {
+    std::cout << e << ' ';
+  }
+  std::cout << std::endl;
+}
 
+#include <algorithm>
+#include <iostream>
+#include <string>
 
+void comb(int N, int K)
+{
+  std::string bitmask(K, 1); // K leading 1's
+  bitmask.resize(N, 0);      // N-K trailing 0's
+
+  // print integers and permute bitmask
+  do
+  {
+    for (int i = 0; i < N; ++i) // [0..N-1] integers
+    {
+      if (bitmask[i])
+        std::cout << " " << i;
+    }
+    std::cout << std::endl;
+  } while (std::prev_permutation(bitmask.begin(), bitmask.end()));
+}
 
 int main()
 {
-
-  return 0;
+  comb(5, 3);
 }
