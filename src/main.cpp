d994b4b7f4eab925a1bb10e4fcdc7913225b2c5e
radishkill <radishkill@gmail.com>
2022-12-05 16:12:08 +0800

    modify

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,22 @@
 #include <bits/stdc++.h>
-#include <iostream>
-#include <algorithm>
-
-void print(const auto &a)
-{
-  for (const auto e : a)
-  {
-    std::cout << e << ' ';
-  }
-  std::cout << std::endl;
-}
+#include <gflags/gflags.h>
+#include <glog/logging.h>
 
 #include <algorithm>
 #include <iostream>
 #include <string>
 
+DEFINE_bool(gpu, false, "Use GPU to brew Caffe");
+
 void comb(int N, int K)
 {
-  std::string bitmask(K, 1); // K leading 1's
-  bitmask.resize(N, 0);      // N-K trailing 0's
+  std::string bitmask(K, 1);  // K leading 1's
+  bitmask.resize(N, 0);       // N-K trailing 0's
 
   // print integers and permute bitmask
   do
   {
-    for (int i = 0; i < N; ++i) // [0..N-1] integers
+    for (int i = 0; i < N; ++i)  // [0..N-1] integers
     {
       if (bitmask[i])
         std::cout << " " << i;
@@ -32,7 +25,21 @@ void comb(int N, int K)
   } while (std::prev_permutation(bitmask.begin(), bitmask.end()));
 }
 
-int main()
+int main(int argc, char** argv)
 {
-  comb(5, 3);
+  using namespace std;
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  FLAGS_log_dir = "./";
+
+  google::InitGoogleLogging(argv[0]);
+  google::LogToStderr();
+  LOG(INFO) << "test";
+#ifdef USE_GPU
+  LOG(INFO) << "Add use GPU : " << USE_GPU;
+#endif
+
+  google::FlushLogFiles(google::GLOG_INFO);
+  gflags::ShutDownCommandLineFlags();
+  return 0;
 }
